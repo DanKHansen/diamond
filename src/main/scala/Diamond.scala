6a3f9578897e79121025f3f0a36cc912a62c4163
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-04-24 09:58:00 +0200

    refactoring

--- src/main/scala/Diamond.scala
+++ src/main/scala/Diamond.scala
@@ -1,9 +1,7 @@
 object Diamond:
    def rows(c: Char): List[String] =
-      val iterations = c - 65
-      val ls =
-         for x <- 'A' to c
-         yield
-            val leftside = " " * (iterations - (x - 65)) + x + " " * (x - 65)
-            leftside ++ leftside.reverse.drop(1)
-      ls.toList ++ ls.reverse.drop(1)
+      val diamond = for (x <- 'A' to c) yield
+         val spaces = " " * (c - x)
+         val middle = if x == 'A' then "" else " " * (2 * (x - 'A') - 1)
+         spaces + x + middle + (if x == 'A' then "" else x) + spaces
+      diamond.toList ++ diamond.reverse.tail
